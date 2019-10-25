77789a15c99515ebe07bd87e3119ca2339c49829
Your Name <1282632965@qq.com>
2019-10-25 21:09:30 +0800

    项目初始化完毕

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+
+// main.js的作用是入口文件
+// 职责1：创建一个根实例
+// 实例2: 负责全局范围的依赖导入
