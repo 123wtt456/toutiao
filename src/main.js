0b954b3ec75cb4e8627e924820d5e62c4583aa6d
Your Name <1282632965@qq.com>
2019-10-26 11:42:05 +0800

    登录模块 基础布局 绘制表单

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
+import router from '@/router'
+// ElementUI 是一个插件  集成了很多组件
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 // 根实例
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
 
