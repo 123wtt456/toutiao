80b291009e559b02b3a1ce944ea769ebfeeb9d20
Your Name <1282632965@qq.com>
2019-10-28 10:15:44 +0800

    惊醒校验和登录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import router from '@/router'
+import axios from './api/index'
+Vue.prototype.$http = axios
+
 // ElementUI 是一个插件  集成了很多组件
 Vue.use(ElementUI)
 
@@ -13,6 +16,6 @@ new Vue({
   render: h => h(App)
 }).$mount('#app')
 
-// main.js的作用是入口文件
+// main.js的作用是入口文件5
 // 职责1：创建一个根实例
 // 实例2: 负责全局范围的依赖导入
